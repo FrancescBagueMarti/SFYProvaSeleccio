92825fa6f6ab7ad240b522c30b96379bbf4bff11
Francesc Bague Marti <francesc.bague@gmail.com>
2022-02-26 19:27:37 +0100

    Avance en el proyecto
    Creación y finalización de OnBoardActivity y creación y inicio de SplashScreenActivity

--- app/src/main/java/com/example/sfy_provaseleccio/OnBoardActivity.java
+++ app/src/main/java/com/example/sfy_provaseleccio/OnBoardActivity.java
@@ -1,35 +1,36 @@
 package com.example.sfy_provaseleccio;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 
-import com.example.sfy_provaseleccio.adapters.SliderAdapter;
-import com.google.android.material.tabs.TabLayout;
 
-import org.jetbrains.annotations.NotNull;
-
-public class OnBoardActivity extends AppCompatActivity {
-
-    private ViewPager viewPager;
-    private TabLayout tabLayout;
-
-    private SliderAdapter sliderAdapter;
-    private Button btnAtras;
-    private Button btnSiguiente;
+public class OnBoardActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_on_board);
 
+        final Button btnStart = findViewById(R.id.btnStart);
 
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeActivity(view.getContext(), MainActivity.class);
+            }
+        });
+
+    }
+
+    private void changeActivity(Context currentActivity, Class<?> nextActivity) {
+        Intent intent = new Intent(currentActivity, nextActivity);
+        startActivity(intent);
+        finish();
     }
 }
---
+++ app/src/main/java/com/example/sfy_provaseleccio/SplashScreenActivity.java
@@ -0,0 +1,13 @@
+package com.example.sfy_provaseleccio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
